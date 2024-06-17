d1fe387939819b5cf7060ea486fb77fce17bfce7
doduwole <doduwole@student.42wolfsburg.de>
2024-06-17 09:55:36 +0200

    [refactored] API mocking implemented

--- filesystemDB/__tests__/mock.test.js
+++ filesystemDB/__tests__/mock.test.js
@@ -3,15 +3,9 @@ import request from "supertest";
 import injectApp from "../app.js";
 import { jest } from "@jest/globals";
 
-const getItems = jest.fn((req, res) =>
-  res.status(200).json([req.body])
-);
-const createItem = jest.fn((req, res) =>
-  res.status(201).json(req.body)
-);
-const updateItem = jest.fn((req, res) =>
-  res.status(200).json(req.body)
-);
+const getItems = jest.fn((req, res) => res.status(200).json([req.body]));
+const createItem = jest.fn((req, res) => res.status(201).json(req.body));
+const updateItem = jest.fn((req, res) => res.status(200).json(req.body));
 const deleteItem = jest.fn((req, res) =>
   res.status(200).json({ message: "Item deleted" })
 );
@@ -23,7 +17,6 @@ const app = injectApp({
   deleteItem,
 });
 
-
 describe("Items API", () => {
   beforeEach(() => {
     getItems.mockClear();
@@ -33,31 +26,46 @@ describe("Items API", () => {
   });
 
   test("should call getItems", async () => {
-    await request(app).get("/api/v1/items");
+    const { body, status } = await request(app).get("/api/v1/items");
+
     expect(getItems.mock.calls.length).toBe(1);
+    expect(status).toBe(200);
+    expect(body).toEqual([{}]);
   });
 
-  it('should call createItem', async () => {
-    const itemList = [{ id: 1, name: 'Item 1' }, { id: 2, name: 'Item 2' }];
+  it("should call createItem", async () => {
+    const itemList = [
+      { id: 1, name: "Item 1" },
+      { id: 2, name: "Item 2" },
+    ];
+
     for (const item of itemList) {
       createItem.mockClear();
+      const {status, body} = await request(app).post("/api/v1/items").send(item);
 
-      await request(app).post('/api/v1/items').send(item);
       expect(createItem.mock.calls.length).toBe(1);
       expect(createItem.mock.calls[0][0].body).toEqual(item);
+      expect(status).toEqual(201);
+      expect(body).toHaveProperty('id', item.id);
+      expect(item).toHaveProperty('name', item.name);
     }
   });
 
-  it('should update an existing item', async () => {
-    const item = { id: 1, name: 'Updated Item 1' };
-    await request(app).put('/api/v1/items/1').send(item);
+  it("should update an existing item", async () => {
+    const item = { id: 1, name: "Updated Item 1" };
+    const { body, status } = await request(app).put("/api/v1/items/1").send(item);
 
     expect(updateItem.mock.calls.length).toBe(1);
     expect(updateItem.mock.calls[0][0].body).toEqual(item);
+    expect(status).toEqual(200);
+    expect(body).toHaveProperty('name', item.name);
+    expect(body).toHaveProperty('id', item.id);
   });
 
-  it('should delete an existing item', async () => {
-    const res = await request(app).delete('/api/v1/items/1');
+  it("should delete an existing item", async () => {
+    const { status, body } = await request(app).delete("/api/v1/items/1");
     expect(deleteItem.mock.calls.length).toBe(1);
+    expect(status).toEqual(200);
+    expect(body).toHaveProperty('message', 'Item deleted');
   });
 });
