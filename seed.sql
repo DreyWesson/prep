1cf86e84fca41227fc13715d842f3fb06ebf47e2
doduwole <doduwole@student.42wolfsburg.de>
2024-06-25 20:22:33 +0200

    postgresql set up done but there are many missing api implementations and also some test will fail v.2

--- seed.sql
+++ seed.sql
@@ -7,9 +7,18 @@ CREATE TABLE IF NOT EXISTS "item" (
   "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- INSERT
-INSERT INTO "item" ("name", "description")
-VALUES ('Item 1', 'Description 1');
 
--- SELECT
+-- READ
 SELECT * FROM "item";
+SELECT * FROM "item" WHERE "id" = 1;
+SELECT * FROM "item" WHERE "name" = 'Item 1';
+
+-- CREATE
+INSERT INTO "item" ("name", "description")
+VALUES ('Item 2', 'Description 2') RETURNING *;
+
+-- UPDATE
+UPDATE "item" SET "name" = 'Item 3' WHERE "id" = 2 RETURNING *;
+
+-- DELETE
+DELETE FROM "item" WHERE "id" = 2;
